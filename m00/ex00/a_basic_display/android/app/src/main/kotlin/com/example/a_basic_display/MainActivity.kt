09f0aef6a4633aa6dcd8f8629b0be91c11004656
xmatute- <xmatute-@student.42urduliz.com>
2025-03-12 21:48:49 +0100

    wip

--- m00/ex00/a_basic_display/android/app/src/main/kotlin/com/example/a_basic_display/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.a_basic_display
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- m00/ex01/hola_holita/android/app/src/main/kotlin/com/example/hola_holita/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.hola_holita
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
