920237a5601c68e547d02f60f2f3423b72b3d27a
xmatute- <xmatute-@student.42urduliz.com>
2025-03-21 15:12:08 +0100

    wip

---
+++ m02/ex00/medium_weather_app/lib/main.dart
@@ -0,0 +1,165 @@
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+
+
+Future<Position> _determinePosition() async {
+  bool serviceEnabled;
+  LocationPermission permission;
+
+  // Test if location services are enabled.
+  serviceEnabled = await Geolocator.isLocationServiceEnabled();
+  if (!serviceEnabled) {
+    // Location services are not enabled don't continue
+    // accessing the position and request users of the 
+    // App to enable the location services.
+    return Future.error('Location services are disabled.');
+  }
+
+  permission = await Geolocator.checkPermission();
+  if (permission == LocationPermission.denied) {
+    permission = await Geolocator.requestPermission();
+    if (permission == LocationPermission.denied) {
+      // Permissions are denied, next time you could try
+      // requesting permissions again (this is also where
+      // Android's shouldShowRequestPermissionRationale 
+      // returned true. According to Android guidelines
+      // your App should show an explanatory UI now.
+      return Future.error('Location permissions are denied');
+    }
+  }
+  
+  if (permission == LocationPermission.deniedForever) {
+    // Permissions are denied forever, handle appropriately. 
+    return Future.error(
+      'Location permissions are permanently denied, we cannot request permissions.');
+  } 
+
+  // When we reach here, permissions are granted and we can
+  // continue accessing the position of the device.
+  return await Geolocator.getCurrentPosition();
+}
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Weather App',
+      theme: ThemeData(
+        colorScheme: ColorScheme.dark(
+          primary: Colors.blue,
+        ),
+      ),
+      home: const MyHomePage(title: 'Weather App'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+  String _location = '';
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 3, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
+  void _updateLocation(String location) {
+    setState(() {
+      _location = location;
+    });
+  }
+
+  void _geolocate() {
+    Future<Position> position = _determinePosition();
+    position.then((value) => 
+      setState(() {
+      _updateLocation(value.toString());
+    })).catchError((error) {
+      setState(() {
+        print('Error: $error');
+        _updateLocation(error.toString());
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        bottom: PreferredSize(
+          preferredSize: const Size.fromHeight(42.0),
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: TextField(
+                        decoration: const InputDecoration(
+                          hintText: 'Search...',
+                          icon: Icon(Icons.search),
+                        ),
+                        // onChanged: _updateLocation,
+                        onSubmitted: _updateLocation,
+                      ),
+                    ),
+                    IconButton(
+                      icon: const Icon(Icons.location_on),
+                      onPressed: () {
+                        // Geolocation button action
+                        print('Geolocation button pressed');
+                        _geolocate();
+                      },
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      body: TabBarView(
+        controller: _tabController,
+        children: [
+          Center(child: Text('Currently\n$_location', textAlign: TextAlign.center)),
+          Center(child: Text('Today\n$_location', textAlign: TextAlign.center)),
+          Center(child: Text('Weekly\n$_location', textAlign: TextAlign.center)),
+        ],
+      ),
+      bottomNavigationBar: BottomAppBar(
+        child: TabBar(
+          controller: _tabController,
+          tabs: const [
+            Tab(icon: Icon(Icons.window), text: 'Currently'),
+            Tab(icon: Icon(Icons.today), text: 'Today'),
+            Tab(icon: Icon(Icons.calendar_view_week), text: 'Weekly'),
+          ],
+        ),
+      ),
+    );
+  }
+}
